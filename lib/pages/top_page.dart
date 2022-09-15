190d53dcc79973d502b142f1bfa7705e9a529f00
macbook <macbook@appurunoMacBook-Pro.local>
2022-09-15 18:55:49 +0900

    easy to see for code

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qiita_client_yukiy/ui_components/thin_long_rounded_button.dart';
 
 class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
@@ -26,68 +27,48 @@ class _TopPageState extends State<TopPage> {
             color: Colors.black.withOpacity(0.2),
           ),
           Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                const SizedBox(
-                  height: 220,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Qiita Feed App',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 36,
-                      fontFamily: 'Pacifico-Regular',
-                    ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(
+                height: 220,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'Qiita Feed App',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontFamily: 'Pacifico-Regular',
                   ),
                 ),
-                Container(
-                  alignment: Alignment.center,
-                  child: const Text(
-                    '-PlayGround-',
-                    style: TextStyle(
-                      letterSpacing: 0.25,
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  '-PlayGround-',
+                  style: TextStyle(
+                    letterSpacing: 0.25,
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                const Spacer(),
-                Container(
-                  height: 50,
-                  margin: const EdgeInsets.symmetric(horizontal: 24),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        textStyle: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        backgroundColor: const Color.fromRGBO(70, 131, 0, 1),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        )),
-                    child: const Text('ログイン'),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(34),
-                  width: 327,
-                  height: 50,
-                  child: TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        textStyle: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        )),
-                    child: const Text('ログインせずに利用する'),
-                  ),
-                ),
-              ])
+              ),
+              const Spacer(),
+              ThinLongRoundedButton(
+                text: 'ログイン',
+                backgroundColor: const Color(0xFF468300),
+                onPressed: () {},
+              ),
+              ThinLongRoundedButton(
+                text: 'ログインせずに利用する',
+                backgroundColor: Colors.transparent,
+                onPressed: () {},
+              ),
+            ],
+          ),
         ],
       ),
     );
