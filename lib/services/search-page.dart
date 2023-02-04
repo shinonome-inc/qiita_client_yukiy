fdeb614c8fabd0d807bbecdbf97112099ae60e24
macbook <macbook@MacBook-Air-5.local>
2023-02-04 16:43:20 +0900

    WebViewScreen.dartのファイルを変更

---
+++ lib/services/search-page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  SearchPage({Key? key}) : super(key: key);
+  final List<String> articleList = [
+
+  ]
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
