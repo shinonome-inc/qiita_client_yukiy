f78c7d6eb7ee828cc50141eb8b4ac6f43d2746bb
macbook <macbook@MacBook-Air-5.local>
2023-03-05 12:00:33 +0900

    データ取得完了

--- lib/pages/tag_detail_list_page.dart
+++ lib/pages/tag_detail_list_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:qiita_client_yukiy/ui_components/article_list_view.dart';
 import 'package:qiita_client_yukiy/ui_components/upper_bar.dart';
+
+import '../models/article.dart';
+import '../services/qiita_client.dart';
 
 class TagDetailListPage extends StatefulWidget {
   const TagDetailListPage({Key? key, required this.tagName}) : super(key: key);
@@ -10,12 +14,38 @@ class TagDetailListPage extends StatefulWidget {
 }
 
 class _TagDetailListPageState extends State<TagDetailListPage> {
+  List<Article> listArticle = [];
+  late Future<List<Article>> futureArticles;
+  @override
+  void initState() {
+    futureArticles = QiitaClient.fetchArticle("");
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: UpperBar(
         appBarText: widget.tagName,
         textField: const TextField(),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Article>>(
+          future: futureArticles,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              listArticle = snapshot.data!;
+              return ArticleListView(articles: listArticle);
+            } else if (snapshot.hasError) {
+              return const Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              );
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
