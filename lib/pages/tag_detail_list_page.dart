332c75c1620aac6781f467ec11395f9baf272d9c
macbook <macbook@MacBook-Air-5.local>
2023-03-11 11:41:11 +0900

    futureBuilder内を簡潔化

--- lib/pages/tag_detail_list_page.dart
+++ lib/pages/tag_detail_list_page.dart
@@ -31,29 +31,17 @@ class _TagDetailListPageState extends State<TagDetailListPage> {
   }
 
   Future<void> _fetchTagData() async {
+    print('TagPageNumber is $pageNumber');
+    print("TagFetched");
     _isLoading = true;
     futureTagDetails = QiitaClient.fetchTagDetail(widget.tagName!, pageNumber);
     await Future.delayed(const Duration(seconds: 1));
-    listTagDetail.addAll(await futureTagDetails!);
-    print("TagFetched");
-    print('TagPageNumber is $pageNumber');
-    print('タグ表示件数: ${listTagDetail.length}');
-
-    if (mounted) {
-      setState(
-        () {
-          if (listTagDetail.isNotEmpty) {
-            pageNumber++;
-          }
-        },
-      );
-    }
   }
 
   void _scrollListener() async {
     double positionRate =
         _scrollController.offset / _scrollController.position.maxScrollExtent;
-    const double threshold = 0.8;
+    const double threshold = 0.9;
     if (positionRate > threshold && !_isLoading) {
       if (mounted) {
         setState(() {
@@ -79,22 +67,20 @@ class _TagDetailListPageState extends State<TagDetailListPage> {
         child: FutureBuilder<List<Article>>(
           future: futureTagDetails,
           builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                _isLoading) {
+              _isLoading = false;
+              pageNumber += 1;
+              listTagDetail.addAll(snapshot.data!);
+              print('タグ表示件数: ${listTagDetail.length}');
+            }
             if (snapshot.hasData) {
-              listTagDetail = snapshot.data!;
-              return ListView(
-                children: [
-                  SizedBox(
-                    height: screenHeight,
-                    width: screenWidth,
-                    child: ArticleListView(
-                      articles: listTagDetail,
-                      scrollController: _scrollController,
-                      itemCount: _isLoading
-                          ? listTagDetail.length + 1
-                          : listTagDetail.length,
-                    ),
-                  ),
-                ],
+              return ArticleListView(
+                articles: listTagDetail,
+                scrollController: _scrollController,
+                itemCount: _isLoading
+                    ? listTagDetail.length + 1
+                    : listTagDetail.length,
               );
             } else if (snapshot.hasError) {
               return const Icon(
