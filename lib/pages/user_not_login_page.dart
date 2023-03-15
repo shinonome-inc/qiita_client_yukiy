22271ad6803bb364fbeebce652caeb7a6e7e8852
macbook <macbook@MacBook-Air-5.local>
2023-03-15 17:46:06 +0900

    未ログイン時のUIを作成

--- lib/pages/user_not_login_page.dart
+++ lib/pages/user_not_login_page.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:qiita_client_yukiy/pages/top_page.dart';
 import 'package:qiita_client_yukiy/ui_components/upper_bar.dart';
 
+import '../ui_components/thin_long_rounded_button.dart';
+
 class UserNotLoginPage extends StatelessWidget {
-  const UserNotLoginPage({Key? key}) : super(key: key);
+  UserNotLoginPage({Key? key}) : super(key: key);
+  late double screenHeight = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: UpperBar(
+    screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: const UpperBar(
         appBarText: 'MyPage',
         textField: TextField(),
+      ),
+      body: SizedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 6),
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "ログインが必要です",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 6),
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'マイページの機能を利用するには',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+            const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'ログインを行っていただく必要があります。',
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 247),
+              child: ThinLongRoundedButton(
+                text: 'ログインする',
+                backgroundColor: const Color(0xFF74C13A),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const TopPage(),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
