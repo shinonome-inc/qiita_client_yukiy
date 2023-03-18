f1825474b168863b1dd4ff5d78278db64d63bf2b
macbook <macbook@MacBook-Air-5.local>
2023-03-18 12:39:58 +0900

    不要なSizedboxを削除

--- lib/pages/user-not-login-page.dart
+++ lib/pages/user-not-login-page.dart
@@ -18,58 +18,56 @@ class UserNotLoginPage extends StatelessWidget {
         appBarText: 'MyPage',
         textField: TextField(),
       ),
-      body: SizedBox(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: screenHeight / 4, bottom: 6),
-              child: const Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "ログインが必要です",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 6),
-              child: const Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'マイページの機能を利用するには',
-                  style: TextStyle(
-                    fontSize: 12,
-                  ),
-                ),
-              ),
-            ),
-            const Align(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: screenHeight / 4, bottom: 6),
+            child: const Align(
               alignment: Alignment.center,
               child: Text(
-                'ログインを行っていただく必要があります。',
+                "ログインが必要です",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 6),
+            child: const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'マイページの機能を利用するには',
                 style: TextStyle(
                   fontSize: 12,
                 ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(top: screenHeight / 3),
-              child: ThinLongRoundedButton(
-                text: 'ログインする',
-                backgroundColor: const Color(0xFF74C13A),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const TopPage(),
-                    ),
-                  );
-                },
+          ),
+          const Align(
+            alignment: Alignment.center,
+            child: Text(
+              'ログインを行っていただく必要があります。',
+              style: TextStyle(
+                fontSize: 12,
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: screenHeight / 3),
+            child: ThinLongRoundedButton(
+              text: 'ログインする',
+              backgroundColor: const Color(0xFF74C13A),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TopPage(),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
