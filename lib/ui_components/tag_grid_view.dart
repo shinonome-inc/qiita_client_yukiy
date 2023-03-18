6f6d3e026235683f789def839ea9d6a387846848
macbook <macbook@MacBook-Air-5.local>
2023-03-18 11:31:50 +0900

    不要な箇所の削除

--- lib/ui_components/tag_grid_view.dart
+++ lib/ui_components/tag_grid_view.dart
@@ -14,49 +14,28 @@ class TagGridView extends StatelessWidget {
   final int itemCount;
   final List<Tag> tagList;
   final ScrollController scrollController;
-  late bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
     double columnWidth = 170;
     int columnCount = deviceSize.width ~/ columnWidth;
-    return Column(
-      children: [
-        Expanded(
-          child: GridView.builder(
-            controller: scrollController,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              // 横1行あたりに表示するWidgetの数
-              crossAxisCount: columnCount,
-            ),
-            itemCount: itemCount,
-            itemBuilder: (BuildContext context, int index) {
-              final tag = tagList[index];
-              if (index == 0) {
-                _isLoading = true;
-                return TagGridViewCell(
-                  tag: tag,
-                );
-              }
-              return TagGridViewCell(tag: tag);
-            },
-          ),
-        ),
-        if (_isLoading) _loadingView(),
-      ],
+    return GridView.builder(
+      controller: scrollController,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        // 横1行あたりに表示するWidgetの数
+        crossAxisCount: columnCount,
+      ),
+      itemCount: itemCount,
+      itemBuilder: (BuildContext context, int index) {
+        final tag = tagList[index];
+        if (index == 0) {
+          return TagGridViewCell(
+            tag: tag,
+          );
+        }
+        return TagGridViewCell(tag: tag);
+      },
     );
   }
-}
-
-Widget _loadingView() {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: const [
-      CircularProgressIndicator(),
-      SizedBox(
-        height: 8,
-      )
-    ],
-  );
 }
