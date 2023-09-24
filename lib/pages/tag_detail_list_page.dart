2b0f9f749e0d36215f87503df3cc70dda8848b7c
Yuki0316 <chatoranta@gmail.com>
2023-09-24 15:58:36 +0900

    Tag詳細ページ修正

--- lib/pages/tag_detail_list_page.dart
+++ lib/pages/tag_detail_list_page.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qiita_client_yukiy/ui_components/article_list_view.dart';
 import 'package:qiita_client_yukiy/ui_components/upper_bar.dart';
 
 import '../models/article.dart';
 import '../services/qiita_client.dart';
+import 'error_page.dart';
 
 class TagDetailListPage extends StatefulWidget {
   const TagDetailListPage({Key? key, required this.tagName}) : super(key: key);
@@ -20,6 +20,8 @@ class _TagDetailListPageState extends State<TagDetailListPage> {
   late ScrollController _scrollController;
   late bool _isLoading = true;
   int pageNumber = 1;
+  bool showError = false;
+  bool isNetworkError = false;
 
   @override
   void initState() {
@@ -30,11 +32,30 @@ class _TagDetailListPageState extends State<TagDetailListPage> {
   }
 
   Future<void> _fetchTagData() async {
+    List<Article> newTagDetail = [];
+    setState(() {
+      _isLoading = true;
+      isNetworkError = false;
+    });
+    try {
+      newTagDetail =
+          await QiitaClient.fetchTagDetail(widget.tagName!, pageNumber);
+    } catch (e) {
+      setState(() {
+        isNetworkError = true;
+      });
+      throw Exception(e);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+    setState(() {
+      listTagDetail.addAll(newTagDetail);
+      showError = newTagDetail.isEmpty ? true : false;
+    });
     print('TagPageNumber is $pageNumber');
     print("TagFetched");
-    _isLoading = true;
-    futureTagDetails = QiitaClient.fetchTagDetail(widget.tagName!, pageNumber);
-    await Future.delayed(const Duration(seconds: 1));
   }
 
   void _scrollListener() async {
@@ -42,8 +63,31 @@ class _TagDetailListPageState extends State<TagDetailListPage> {
         _scrollController.offset / _scrollController.position.maxScrollExtent;
     const double threshold = 0.9;
     if (positionRate > threshold && !_isLoading) {
+      if (mounted) {
+        setState(() {
+          _isLoading = true;
+        });
+      }
+      pageNumber++;
       await _fetchTagData();
     }
+  }
+
+  void _refresh() {
+    setState(() {
+      listTagDetail = [];
+      pageNumber = 1;
+    });
+  }
+
+  Future<void> _onRefresh() async {
+    if (showError) {
+      setState(() {
+        showError = false;
+      });
+    }
+    _refresh();
+    _fetchTagData();
   }
 
   @override
@@ -54,35 +98,20 @@ class _TagDetailListPageState extends State<TagDetailListPage> {
         textField: const TextField(),
         automaticallyImplyLeading: true,
       ),
-      body: Center(
-        child: FutureBuilder<List<Article>>(
-          future: futureTagDetails,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done &&
-                _isLoading) {
-              _isLoading = false;
-              pageNumber += 1;
-              listTagDetail.addAll(snapshot.data!);
-              print('タグ表示件数: ${listTagDetail.length}');
-            }
-            if (snapshot.hasData) {
-              return ArticleListView(
-                articles: listTagDetail,
-                scrollController: _scrollController,
-                itemCount: _isLoading
-                    ? listTagDetail.length + 1
-                    : listTagDetail.length,
-                showGreyPart: true,
-              );
-            } else if (snapshot.hasError) {
-              return const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              );
-            }
-            return const CupertinoActivityIndicator();
-          },
+      body: RefreshIndicator(
+        onRefresh: _onRefresh,
+        child: Center(
+          child: isNetworkError
+              ? ErrorPage(
+                  onTapped: _onRefresh,
+                )
+              : ArticleListView(
+                  articles: listTagDetail,
+                  scrollController: _scrollController,
+                  itemCount: _isLoading
+                      ? listTagDetail.length + 1
+                      : listTagDetail.length,
+                ),
         ),
       ),
     );
